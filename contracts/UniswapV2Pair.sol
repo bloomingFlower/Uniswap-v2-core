6177443f8e6fe3c8b50b173f565affc07d1f39b3
AAII <sinsunby@gmail.com>
2022-10-26 23:48:50 +0900

    test

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -102,7 +102,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         emit Sync(reserve0, reserve1);
     }
 
-    // check out
+    // check out test
     // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
     function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
         address feeTo = IUniswapV2Factory(factory).feeTo();
